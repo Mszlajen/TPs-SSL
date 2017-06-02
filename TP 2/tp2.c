07a89caec3c5519bcee9bed4a98ea1e36d92f4cb
mszlajen <mszlajen@est.frba.utn.edu.ar>
2017-06-02 17:13:44 -0300

    falta arreglar cosas

--- TP 2/tp2.c
+++ TP 2/tp2.c
@@ -1,112 +1,113 @@
 #include <stdio.h>
+#include <string.h>
 
-int automataPalabrasReservadas (char[]);
-int valoresPalabrasResevadas (char);
-int automataOperadores (char[]);
-int valoresOperadores (char);
-int automataIdentificadores (char[]);
-int valoresIdentificadores (char);
-int automataConstantes (char[]);
-int valoresConstantes (char);
-int automataPuntuacion (char[]);
-int valoresPuntuacion (char);
-int automataToken(char[]);
+#define tam 10
 
-int main ()
+//int automata (int columnas, int, int [][columnas], char [], int [], int, char []);
+int automata (int *[], char [], int [], int, char []);
+void generalizarCaracteres (char [], char [], char[3]);
+int esTerminal (int, int []);
+int posCaracter (char [], char);
+
+int automataReservadas (char []);
+int automataConstantes (char []);
+int automataOperadores (char []);
+int automataPuntuacion (char []);
+int automataIdentificadores (char []);
+
+void main ()
 {
-    char palabra[10];
-    int est;
-
-    scanf("%s", palabra);
-    est = automataToken(palabra);
-
-    switch(est)
-    {
-    case 0:
-        printf("Palabra Reservada");
-        break;
-    case 1:
-        printf("Operador");
-        break;
-    case 2:
-        printf("Identificador");
-        break;
-    case 3:
-        printf("Constante");
-        break;
-    case 4:
-        printf("Caracter de Puntuacion");
-        break;
-    default:
-        printf("No es un token");
-    }
-    return 0;
-}
-
-int automataToken (char palabra[])
-{
-    int est = 0, token;
-    do
-    {
-        switch(est)
+    char cadena[tam + 1];
+    int i, terminado = 0;
+    scanf("%s", cadena);
+    for (i = 0; i <= 5 && !terminado; i++)
+        switch(i)
         {
-        case 0:
-            token = automataPalabrasReservadas(palabra);
-            break;
-        case 1:
-            token = automataOperadores(palabra);
-            break;
-        case 2:
-            token = automataIdentificadores(palabra);
-            break;
-        case 3:
-            token = automataConstantes(palabra);
-            break;
-        case 4:
-            token = automataPuntuacion(palabra);
-            break;
-        default:
-            token = 1;
+            case 0:
+                if(terminado = automataReservadas(cadena))
+                    printf("Palabra Reservada");
+                break;
+            case 1:
+                if(terminado = automataOperadores(cadena))
+                    printf("Operador");
+                break;
+            case 2:
+                if(terminado = automataPuntuacion(cadena))
+                    printf("Caracter de Puntuacion");
+                break;
+            case 3:
+                if(terminado = automataIdentificadores(cadena))
+                    printf("Identificador");
+                break;
+            case 4:
+                if(terminado = automataConstantes(cadena))
+                    printf("Constante");
+                break;
+            default:
+                printf("No es lexema");
         }
-        est++;
-    }while(!token);
-    return est - 1;
 }
 
-int valoresPalabrasResevadas (char caracter)
+int automata (int *transiciones[], char caracteres[], int terminales[], int estVacio, char cadena[])
 {
-    switch (caracter)
+    int i, estado = 0, valorCaracter;
+    for(i = 0; cadena[i] != '\0'; i++)
     {
-    case 'i':
-        return 0;
-    case 'f':
-        return 1;
-    case 'e':
-        return 2;
-    case 'l':
-        return 3;
-    case 's':
-        return 4;
-    case 'w':
-        return 5;
-    case 'h':
-        return 6;
-    case 'n':
-        return 7;
-    case 't':
-        return 8;
-    case 'o':
-        return 9;
-    case 'a':
-        return 10;
-    default:
-        return -1;
+        valorCaracter = posCaracter(caracteres, cadena[i]);
+        if(valorCaracter >= 0)
+            estado = (transiciones + estado * sizeof(int))[valorCaracter];
+        else
+            estado = estVacio;
     }
+    if(esTerminal(estado, terminales))
+        return 1;
+    else
+        return 0;
 }
 
-int automataPalabrasReservadas (char cadena[])
+int esTerminal (int estado, int terminales[])
 {
-    int i, valorCaracter, estado = 0;
+    int i, resultado = 0;
+
+    for(i = 0; !resultado && terminales[i] != -1; i++)
+    {
+        if(terminales[i] == estado)
+            resultado = 1;
+    }
+    return resultado;
+}
+
+int posCaracter (char cadena[], char caracter)
+{
+    int i;
+
+    for (i = 0; cadena [i] != '\0' && cadena [i] != caracter; i++);
+    if(cadena [i] == '\0')
+        return -1;
+    else
+        return i;
+}
+
+void generalizarCaracteres (char original[], char destino[], char sustitutos[3]) // Sustitutos va [Digito, Mayuscula, Minuscula]
+{
+    int i;
+
+    for(i = 0; original[i] != '\0'; i++)
+    {
+        if('0' <= original[i] && original[i] <= '9')
+            destino[i] = sustitutos[0];
+        else if('A' <= original[i] && original[i] <= 'Z')
+            destino[i] = sustitutos[1];
+        else if('a' <= original[i] && original[i] <= 'z')
+            destino[i] = sustitutos[2];
+        else
+            destino[i] = original[i];
+    }
+    destino[i] = '\0';
+}
+
+int automataReservadas (char cadena[])
+{
     int transiciones[][11] = {{1,9,3,13,13,6,13,13,13,13,13},
                             {13,12,13,13,13,13,13,2,13,13,13},
                             {13,13,13,13,13,13,13,13,12,13,13},
@@ -121,176 +122,69 @@ int automataPalabrasReservadas (char cadena[])
                             {13,13,13,13,13,13,13,13,13,13,2},
                             {13,13,13,13,13,13,13,13,13,13,13},
                             {13,13,13,13,13,13,13,13,13,13,13}};
+    char caracteres [] = {'i', 'f', 'e', 'l', 's', 'w', 'h', 'n', 't', 'f', 'o', 'a'};
+    int terminales [] = {12, -1};
 
-    for(i = 0; cadena[i] != '\0'; i++)
-    {
-        valorCaracter = valoresPalabrasResevadas(cadena[i]);
-        if(valorCaracter >= 0)
-            estado = transiciones[estado][valorCaracter];
-        else
-            estado = 13;
-    }
-    if(estado == 12)
-        return 1;
-    else
-        return 0;
-}
-
-int valoresOperadores (char caracter)
-{
-    switch (caracter)
-    {
-    case '+':
-        return 0;
-    case '-':
-        return 1;
-    case '=':
-        return 2;
-    case '>':
-        return 3;
-    case '!':
-        return 4;
-    case '(':
-        return 5;
-    case ')':
-        return 6;
-    case '&':
-        return 7;
-    case '|':
-        return 8;
-    default:
-        return -1;
-    }
+    return automata(&transiciones, caracteres, terminales, 13, cadena);
+    //return automata(11, 14,transiciones, caracteres, terminales, 1, cadena);
 }
 
 int automataOperadores (char cadena[])
 {
-    int i, valorCaracter, estado = 0;
     int transiciones[][9] = {{5,5,2,5,5,1,6,3,4},
                             {6,6,6,6,6,6,5,6,6},
                             {6,6,5,6,6,6,6,6,6},
                             {6,6,6,6,6,6,6,5,6},
                             {6,6,6,6,6,6,6,6,5},
+                            {6,6,6,6,6,6,6,6,6},
                             {6,6,6,6,6,6,6,6,6}};
+    char caracteres [] = {'+', '-', '=', '>', '!', '(', ')', '&', '|'};
+    int terminales [] = {2, 5, -1};
 
-    for(i = 0; cadena[i] != '\0'; i++)
-    {
-        valorCaracter = valoresOperadores(cadena[i]);
-        if(valorCaracter >= 0)
-            estado = transiciones[estado][valorCaracter];
-        else
-            estado = 6;
-    }
-    if(estado == 2 || estado == 5)
-        return 1;
-    else
-        return 0;
-}
-
-int valoresIdentificadores (char caracter)
-{
-    if('A' <= caracter && caracter <= 'Z')
-        return 0;
-    else if ('a' <= caracter && caracter <= 'z')
-        return 1;
-    else if ('0' <= caracter && caracter <= '9')
-        return 2;
-    else
-        return -1;
-}
-
-int automataIdentificadores (char cadena[])
-{
-    int i, valorCaracter, estado = 0;
-    int transiciones[][3] = {{1, 2, 2},
-                            {1, 1, 1},
-                            {2, 2, 2}};
-
-    for(i = 0; cadena[i] != '\0'; i++)
-    {
-        valorCaracter = valoresIdentificadores(cadena[i]);
-        if(valorCaracter >= 0)
-            estado = transiciones[estado][valorCaracter];
-        else
-            estado = 2;
-    }
-    if(estado == 1)
-        return 1;
-    else
-        return 0;
-}
-
-int valoresConstantes (char caracter)
-{
-    if('0' <= caracter && caracter <= '9')
-        return 0;
-    else
-        switch (caracter)
-        {
-            case '.':
-                return 1;
-            case '+':
-                return 2;
-            case '-':
-                return 3;
-            default:
-                return -1;
-        }
+    return automata(&transiciones, caracteres, terminales, 6, cadena);
+    //return automata(9, 7, transiciones, caracteres, terminales, 2, cadena);
 }
 
 int automataConstantes (char cadena[])
 {
-    int i, valorCaracter, estado = 0;
     int transiciones[][4] = {{4, 2, 1, 1},
                             {4, 2, 5, 5},
                             {3, 5, 5, 5},
                             {3, 5, 5, 5},
                             {4, 3, 5, 5},
                             {5, 5, 5, 5}};
+    char caracteres [] = {'d', '.', '+', '-'};
+    int terminales [] = {3, 4, -1};
+    char cadenaTemp [10], sustitutos[] = {'d', '\0', '\0'};
 
-    for(i = 0; cadena[i] != '\0'; i++)
-    {
-        valorCaracter = valoresConstantes(cadena[i]);
-        if(valorCaracter >= 0)
-            estado = transiciones[estado][valorCaracter];
-        else
-            estado = 6;
-    }
-    if(estado == 3 || estado == 4 || estado == 5)
-        return 1;
-    else
-        return 0;
-}
+    generalizarCaracteres(cadena, cadenaTemp, sustitutos);
 
-int valoresPuntuacion (char caracter)
-{
-
-     switch (caracter)
-    {
-            case ';':
-                return 0;
-            default:
-                return -1;
-    }
+    return automata(&transiciones, caracteres, terminales, 5, cadenaTemp);
+    //return automata (4, 6, transiciones, caracteres, terminales, 3, cadenaTemp);
 }
 
 int automataPuntuacion (char cadena[])
 {
-    int i, valorCaracter, estado = 0;
     int transiciones[][1] = {{1},
                             {2},
                             {2}};
+    char caracteres[] = {';'};
+    int terminales[] = {2, -1};
 
-    for(i = 0; cadena[i] != '\0'; i++)
-    {
-        valorCaracter = valoresPuntuacion(cadena[i]);
-        if(valorCaracter >= 0)
-            estado = transiciones[estado][valorCaracter];
-        else
-            estado = 2;
-    }
-    if(estado == 1)
-        return 1;
-    else
-        return 0;
+    return automata(&transiciones, caracteres, terminales, 1, cadena);
+    //return automata(1, 3, transiciones, caracteres, terminales, 1, cadena);
+}
+
+int automataIdentificadores (char cadena []) //Revisar
+{
+    char transiciones [][3] = {{1, 2, 2},
+                            {1, 1, 1},
+                            {2, 2, 2}};
+    char caracteres [] = {'D', 'M', 'm'};
+    char cadenaTemp [tam + 1];
+    int terminales [] = {1, -1};
+
+    generalizarCaracteres(cadena, cadenaTemp, caracteres);
+
+    return automata(&transiciones, caracteres, terminales, 2, cadenaTemp);
 }
